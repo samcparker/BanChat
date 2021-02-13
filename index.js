f5cfde7b182e98a9d11c2f1a557cbebf3e95ca6d
Sam <samcparker1999@gmail.com>
2021-02-13 18:15:44 +0000

    added port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -65,6 +66,6 @@ io.on('connection', (socket) => {
     });
 });
 
-http.listen(3000, () => {
+http.listen(PORT, () => {
     console.log('listening on http://localhost:3000');
 });
